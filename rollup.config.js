6c32096787cf8fa63912ec68b585d69996e90d7c
karinaDen <88400883+karinaDen@users.noreply.github.com>
2022-06-17 19:13:54 +0300

    add : add TS code

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,13 @@
+import resolve from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+import typescript from "@rollup/plugin-typescript";
+
 export default {
-    input: 'src/main.js',
+    input: 'ts/HW3.ts',
     output: { file: 'bundle.js' },
+    plugins: [
+        resolve({ browser: true }),
+        terser(),
+        typescript(),
+    ],
 };
