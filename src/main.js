79524bc11c3914bde3d5c2d05d4735e5ceafebda
karinaDen <88400883+karinaDen@users.noreply.github.com>
2022-06-17 18:05:57 +0300

    add : factored JS script

---
+++ src/main.js
@@ -0,0 +1,17 @@
+const btn = document.getElementById('btn');
+const pic = document.getElementById('dogImage');
+
+async function f() {
+    const res = await fetch('https://dog.ceo/api/breeds/image/random');
+    const json = await res.json();
+    return json;
+}
+
+btn.addEventListener("click", () => {
+    f().then((message) => {
+pic.src = message.url;
+    });
+})
+
+
+
