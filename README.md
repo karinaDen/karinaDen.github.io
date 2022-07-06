00c1ea30a0770f2c25e724d1d4dba4de4a131a79
karinaDen <88400883+karinaDen@users.noreply.github.com>
2022-07-06 18:40:25 +0300

    add : sveltekit

--- README.md
+++
@@ -1,1 +0,0 @@
-# karinaDen.github.io
--- bundle.js
+++
@@ -1,7 +0,0 @@
-let e = async () => {
-    let e = await fetch("https://random.dog/woof.json"), t = await e.json();
-    return console.log(t.url), t.url
-}, t = document.getElementById("btn"), n = document.getElementById("dogImage");
-t.addEventListener("click", (async t => {
-    t.preventDefault(), e(), n.src = await e()
-}));
