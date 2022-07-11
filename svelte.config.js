99f47226cf5ec3b2ad2f731fae20a9cd29e27b21
karinaDen <88400883+karinaDen@users.noreply.github.com>
2022-07-11 17:47:57 +0300

    add : corrected deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -13,6 +13,10 @@ const config = {
 		// Override http methods in the Todo forms
 		methodOverride: {
 			allowed: ['PATCH', 'DELETE']
+		},
+
+		prerender:{
+			default: true,
 		}
 	}
 };
